1879f60ae2897af4be8c901235b636e40ef691fd
JojoDz <72552227+JojoDz@users.noreply.github.com>
2021-02-17 16:21:58 +0100

    Ajout fonction

--- sketch.js
+++ sketch.js
@@ -1,24 +1,30 @@
 function setup() {
   createCanvas(500, 500);
   background(220);
+  x= width/2; 
+  y= height/2;
+  speed = 2.2;
+}
+
+function drawnCar(x, y) {
+  //dessiner la voiture
+  fill(0)
+  rect(x+10, y-100, 90, 50, 10, 10, 0, 0);
+  fill('green');
+  rect(x, 198, 110, 50);
+  fill('blue');
+  ellipse(x, y, 40, 40);
+  ellipse(x + 110, y, 40, 40);
 }
 
 function draw() {
   background(220);
-  //dessiner la voiture
-  fill(0)
-  rect(car.x+10, car.y-100, 90, 50);
-  fill(0, car.g, 0);
-  rect(car.x, 198, 110, 50);
-  fill(255);
-  ellipse(car.x, car.y, 40, 40);
-  ellipse(car.x + 110, car.y, 40, 40);
-  
+  drawnCar(x, y);
   //condition si la voiture touche le bord droit : reviens en arrière	
-  if (car.x + 110 >= width || car.x < 0) {
+  if (x + 110 >= width || x < 0) {
 	speed = speed * -1;
   }
 
   //vitesse de la voiture
-	car.x = car.x + speed;
+	x = x + speed;
 }
